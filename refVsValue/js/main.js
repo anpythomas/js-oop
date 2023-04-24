67e19aabd65a4686966f3a6353e920d4f71f3c28
Andy <andythomas.py@gmail.com>
2023-04-23 19:29:40 -0500

    Added function passing variable number type

--- refVsValue/js/main.js
+++ refVsValue/js/main.js
@@ -1,9 +1,24 @@
 // By Value
-
 let xValue = 10;
-
 let yValue = xValue;
-
 xValue = 20;
-
 // when we console: x = 20, y = 10
+
+
+// By Reference
+let xRef = {value: 10};
+let yRef = xRef;
+xRef.value = 34;
+// when we console: x = 34, y = 34
+
+
+let number = 10;
+
+function increment(number) {
+    number++;
+    console.log(`${number} inside function`)
+}
+
+increment(number);
+
+console.log(`${number} outside function`)
